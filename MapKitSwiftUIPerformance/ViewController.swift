7c10cfd05220d919235bcbd3a095b932fbd42b36
Vid Tadel <tadelv@gmail.com>
2023-04-11 21:12:06 +0200

    Initial Commit

---
+++ MapKitSwiftUIPerformance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapKitSwiftUIPerformance
+//
+//  Created by Vid Tadel on 4/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
